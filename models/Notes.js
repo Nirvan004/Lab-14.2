3f4cff99fbd3c2256ff36a3a7a27a20d1cc3eca6
Nirvan004 <Nirvanandb2004@gmail.com>
2026-01-21 15:34:36 -0500

    finished task 1 with updating notes.js and fixed one of the routes (post route)

--- models/Notes.js
+++ models/Notes.js
@@ -2,19 +2,24 @@ const { Schema, model } = require('mongoose');
  
 // This is the model you will be modifying
 const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+        user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
  
 const Note = model('Note', noteSchema);
